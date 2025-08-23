a27351f551ea357f7c826511bdb43fdf35cc2dcc
Antoni Ratajczyk <antoni.tytus.ratajczyk@gmail.com>
2025-08-23 14:33:23 +0200

    enable web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
 //#define SERVO_PIN 18       // GPIO pin connected to servo signal
 //#define SERVO_CH  0        // LEDC channel
 //#define SERVO_FREQ 50      // 50 Hz = 20ms period
 //#define SERVO_RES 16       // 16-bit resolution
 bool state= 0;
 int ticks =0;
+
+
 class Servo {
   public:
     int SERVO_PIN_L; // GPIO pin connected to servo signal L
@@ -31,17 +33,13 @@ class Servo {
 
    }
     void go_straight(){
-    
     ledcWrite(SERVO_CH_L, pulseToDuty(2000));
     ledcWrite(SERVO_CH_R, pulseToDuty(1000));
-  
    }
-
    void turn_right(){
     ledcWrite(SERVO_CH_L, pulseToDuty(2000));
     ledcWrite(SERVO_CH_R, pulseToDuty(1600));
    }
-
    void turn_left(){
     ledcWrite(SERVO_CH_L, pulseToDuty(1400));
     ledcWrite(SERVO_CH_R, pulseToDuty(1000));
@@ -50,77 +48,92 @@ class Servo {
     ledcWrite(SERVO_CH_L, pulseToDuty(1000));
     ledcWrite(SERVO_CH_R, pulseToDuty(2000));
    }
-
    void stop(){
     ledcWrite(SERVO_CH_L, pulseToDuty(1500));
     ledcWrite(SERVO_CH_R, pulseToDuty(1500));
-
    }
-
 };
 // Helper to convert microseconds to LEDC duty value
 Servo robocik;
+WiFiServer server(80);
+
+String header;
+
 
 void setup() {
-  
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("dupajasiu","pierdzistasiu");
   Serial.begin(9600);
+  IPAddress myIP = WiFi.softAPIP();
+  
   //Serial.println(maxDuty);
   robocik.setup_n_attach(25,26);
   Serial.println("Enter command: w=forward, s=stop, r=reverse");
   //ledcSetup(SERVO_CH, SERVO_FREQ, SERVO_RES);
   //ledcAttachPin(SERVO_PIN, SERVO_CH);
+  Serial.println(myIP);
+  server.begin();
 }
 
 void loop() {
-   if (Serial.available() > 0) {
-    
-    char cmd = Serial.read();   // read one character
-    if (cmd == 'w') {
-      
-      //Serial.println("Forward!");
-      robocik.go_straight();
-      // trigger forward action
+  WiFiClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);  // echo to USB Serial
+      }
+      if (Serial.available()) {
+        char c = Serial.read();
+        client.write(c);  // send to client
+      }
     }
-    else if (cmd == 'd') { 
-      //Serial.println("Right!");
-      robocik.turn_right();
-      // trigger reverse action
-    }
-    else if (cmd == 'a') {
-      //Serial.println("Right!");
-      robocik.turn_left();
-      // trigger reverse action
-    }
-    else if (cmd == 's') {
-      //Serial.println("Backwards!");
-      robocik.go_backwards();
-    
+    client.stop();
   }
-  ticks = 0;
-   state=0;
-  }
-  else if (Serial.available() == 0 && state == 0 && ticks == 2000 ){
-      ticks = 0;
-      state=1;
-      //Serial.println("Stop!");
-      robocik.stop();
-  }
-  else if (Serial.available() == 0 ){ 
-  //Serial.println("YOMAMA!");
-   ticks ++;
-  }
-  
-      // trigger stop action  
-  
 }
-  ///for (int x = 2000; x > 1500;x= x-100 ){ledcWrite(SERVO_CH, pulseToDuty(x));
- // delay(1500);};// Full speed CW (2000 µs)
-  //robocik.stop(2000);
+   /*
+   if(client){
+    while (client.connected()){
+      if (client.available() > 0) {
+        
+        char cmd = Serial.read();   // read one character
+        if (cmd == 'w') {
+          
+          client.println("Forward!");
+          robocik.go_straight();
+          // trigger forward action
+        }
+        else if (cmd == 'd') { 
+          client.println("Right!");
+          robocik.turn_right();
+          // trigger reverse action
+        }
+        else if (cmd == 'a') {
+          //Serial.println("Right!");
+          robocik.turn_left();
+          // trigger reverse action
+        }
+        else if (cmd == 's') {
+          //Serial.println("Backwards!");
+          robocik.go_backwards();
+        
+      }
+      ticks = 0;
+      state=0;
+      }
+      else if (client.available() == 0 && state == 0 && ticks == 20000 ){
+          ticks = 0;
+          state=1;
+          //Serial.println("Stop!");
+          robocik.stop();
+      }
+      else if (client.available() == 0 ){      // trigger robot stop action   
+      //Serial.println("YOMAMA!");
+      ticks ++;
+      }
 
-  // Stop (1500 µs)
-  //ledcWrite(SERVO_CH, pulseToDuty(1500));
-  //delay(5000);
-
-  // Full speed CCW (1000 µs)
-  //for (int x = 1500; x > 1000;x= x-100 ){ledcWrite(SERVO_CH, pulseToDuty(x));
-  //delay(1500);};// Full speed CW (2000 µs)
+    }
+    client.stop();
+}
+}
+*/
